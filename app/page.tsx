4beab947bd081a94242cf56b224cb44c2c323ecb
Abdulloh20080825 <abdullohkurbonov2008@gmail.com>
2024-10-08 18:50:09 +0500

    Good

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 'use client';
+
 import React, { useState, FormEvent } from 'react';
-import Todo from '@/components/Todo';
 import { v4 as uuid } from 'uuid';
+import Todo from '@/components/Todo';
 
 interface Todo {
 	id: string;
 	title: string;
 	isEdit: boolean;
 }
+
 const Page: React.FC = () => {
 	const [todos, setTodos] = useState<Todo[]>([]);
 	const [value, setValue] = useState<string>('');
@@ -15,24 +17,35 @@ const Page: React.FC = () => {
 	const handleAddTodo = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		if (value.trim() === '') return;
+
 		const newTodo = {
 			id: uuid(),
 			title: value,
 			isEdit: false,
 		};
+
 		setTodos((prev) => [...prev, newTodo]);
 		setValue('');
 	};
 
 	const handleActiveEdit = (id: string) => {
-		const todo = todos.find((x) => x.id === id);
-		if (todo) {
-			todo.isEdit = !todo.isEdit;
-		}
+		setTodos((prevTodos) =>
+			prevTodos.map((todo) =>
+				todo.id === id ? { ...todo, isEdit: !todo.isEdit } : todo
+			)
+		);
 	};
+
+	const handleEditSubmit = (id: string, newTitle: string) => {
+		setTodos((prevTodos) =>
+			prevTodos.map((todo) =>
+				todo.id === id ? { ...todo, title: newTitle, isEdit: false } : todo
+			)
+		);
+	};
+
 	const handleDeleteTodo = (id: string) => {
-		const filteredTodo = todos.filter((x) => x.id !== id);
-		setTodos(filteredTodo);
+		setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
 	};
 
 	return (
@@ -63,7 +76,6 @@ const Page: React.FC = () => {
 						Add Todo
 					</button>
 				</form>
-
 				<ul className='mt-6 space-y-5'>
 					{todos.map((todo) => (
 						<Todo
@@ -71,7 +83,7 @@ const Page: React.FC = () => {
 							todo={todo}
 							handleActiveEdit={handleActiveEdit}
 							handleDelete={handleDeleteTodo}
-							todos={todos}
+							handleEditSubmit={handleEditSubmit}
 						/>
 					))}
 				</ul>
